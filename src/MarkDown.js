8b2386ff26adc01051c99cc027c8eb543a75f7c0
Arsalan <arsalan.ahadi@mail.citytech.cuny.edu>
2020-12-13 04:44:45 -0500

    added design and functionality

--- src/MarkDown.js
+++ src/MarkDown.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkDown from 'react-markdown';
+import { FiMaximize2 } from 'react-icons/fi';
+import { FiMinimize2 } from 'react-icons/fi';
 
 function MarkDown() {
+  const [markDown, setMarkDown] = useState('## Markdown Previewer');
+
+  const handleChange = (e) => {
+    setMarkDown(e.target.value);
+  };
+
   return (
-    <div>
-      <p>MarkDown</p>
+    <div className='container'>
+      <div className='view'>
+        <div className='header'>
+          <p>Editor</p>
+          <button className='btn'>
+            <FiMaximize2 />
+          </button>
+        </div>
+        <textarea
+          id='editor'
+          value={markDown}
+          onChange={handleChange}
+        ></textarea>
+      </div>
+
+      <div className='view' id='preview'>
+        <div className='header'>
+          <p>Preview</p>
+          <button className='btn'>
+            <FiMaximize2 />
+          </button>
+        </div>
+        <ReactMarkDown>{markDown}</ReactMarkDown>
+      </div>
     </div>
   );
 }
